466bb74b9c5e6dccbd1b8580152aa161ef7d036b
Vi Prime <1724913+vprime@users.noreply.github.com>
2024-08-03 02:46:44 -0700

    Improve framerate counter

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -10,6 +10,7 @@ use embedded_graphics::{
 use embedded_graphics::image::{Image, ImageRaw, ImageRawBE, ImageRawLE};
 use embedded_graphics::mono_font::ascii::FONT_6X10;
 use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::primitives::{PrimitiveStyleBuilder, Rectangle, StyledDrawable};
 use embedded_graphics::text::Text;
 use tinybmp::{Bmp, Pixels};
 use crate::BlobcatTest::{base, blank, happy, left, right};
@@ -44,8 +45,6 @@ const DISPLAY_HEIGHT_INCLUSIVE: u32 = DISPLAY_HEIGHT - 1;
 
 impl Default for Game {
     fn default() -> Self {
-
-
         Self {
             time: Instant::now(),
             frame: 0,
@@ -57,6 +56,7 @@ impl Default for Game {
 
 impl Game {
     pub fn update<T: DrawTarget<Color = Rgb565>>(&mut self, display: &mut T, input_state: InputState)  {
+        let clock = Instant::now();
         // Update the game timer
         self.frame += 1;
         let now = self.time.elapsed().as_millis();
@@ -107,11 +107,13 @@ impl Game {
                 }
             }
         }
-        
+
         // Print framerate
         let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-        let text = format!("Frame: {:?}", self.frame);
-        Text::new(&*text, Point::new(0, 10), style).draw(display);
+        let recstyle = PrimitiveStyleBuilder::new().stroke_color(Rgb565::RED).stroke_width(1).fill_color(Rgb565::BLACK).build();
+        let text = format!("R:{:?} Frame:{:?}", clock.elapsed(), self.frame);
+        Rectangle::new(Point::new(0, 0), Size::new(DISPLAY_WIDTH, 20)).draw_styled(&recstyle, display);
+        Text::new(&*text, Point::new(3, 10), style).draw(display);
     }
     
     fn draw_graphic<T: DrawTarget<Color = Rgb565>>(&self, display: &mut T, pixels: &[u8]) {
